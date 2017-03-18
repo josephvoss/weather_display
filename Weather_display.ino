d7bd8a7cdda69214397e57f6cf917da6a55e8d00
Joseph Voss <brosephvossenator@gmail.com>
2017-03-17 21:30:40 -0500

    Changes communication profile to MQTT
    Orginally was reading a webpage that's continually updated via a
    cronjob. Now is updated by a message sent from the arduino client,
    which submits the voltage of the display. This voltage with the
    weather data is submitted to graphite on the server side. The server
    daemon also submits the weather data to sensor to display.

--- Weather_display.ino
+++ Weather_display.ino
@@ -1,21 +1,115 @@
 #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 #include <LiquidCrystal.h>
 
 const char WiFiSSID[] = "2WIRE3442";
 const char WiFiPSK[] = "gentlepotato743";
 
 const int LED_PIN = 2;
+const int BATT_PIN = 8;
 
 const char serverHost[] = "192.168.1.111";
-const unsigned long readRate = 5000; //seconds 600 sec
-unsigned long lastRead = 0;
 
-char readInLine1[32] = "";
-char readInLine2[32] = "";
-
-int mcMode = 0;
-
+WiFiClient wifi_client;
+PubSubClient client(serverHost, 1883, wifi_client);
 LiquidCrystal lcd(D6,D1,D2,D3,D4,D5);
+
+void initHardware()
+{
+  Serial.begin(9600);
+  lcd.begin(16,2);
+  client.setCallback(callback);
+
+  pinMode(BATT_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void connectWiFi()
+{
+  byte ledStatus = LOW;
+
+  // Set WiFi mode to station (as opposed to AP or AP_STA)
+  WiFi.mode(WIFI_STA);
+
+  // WiFI.begin([ssid], [passkey]) initiates a WiFI connection
+  // to the stated [ssid], using the [passkey] as a WPA, WPA2,
+  // or WEP passphrase.
+  WiFi.begin(WiFiSSID, WiFiPSK);
+
+  int wifi_counter = 0;
+  while (WiFi.status() != WL_CONNECTED && wifi_counter < 12)
+  {
+    // Blink the LED
+    digitalWrite(LED_PIN, ledStatus); // Write LED high/low
+    ledStatus = (ledStatus == HIGH) ? LOW : HIGH;
+
+    delay(5000); //wait 5 seconds
+    
+    Serial.println("Not connected");
+    lcd.clear();
+    lcd.print("WiFi Not");
+    lcd.setCursor(0,1);
+    lcd.print("Connected");
+    wifi_counter += 1;
+  }
+  //Serial.println("Wifi connected successfully");
+  lcd.clear();
+  lcd.print("WiFi Connected");
+}
+
+int connectToServer()
+{
+  // LED turns on when we enter, it'll go off when we 
+  // successfully post.
+  digitalWrite(LED_PIN, LOW);
+  int counter = 0;
+  while (counter < 3) 
+  {
+    if (client.connect("ESP8266 Weather"))
+    {
+      client.subscribe("weather_display");
+      Serial.println("Connected to server");
+      digitalWrite(LED_PIN, HIGH);
+      return 1;
+    }
+    counter += 1;
+    delay(5000);
+  } 
+  // If we fail to connect, return 0.
+  Serial.println("Failed to connect to server");
+  lcd.clear();
+  lcd.print("Server Not");
+  lcd.setCursor(0,1);
+  lcd.print("Connected");
+  delay(100);
+  return -1;
+}
+
+bool publishData()
+{
+  int battery_level = analogRead(BATT_PIN);
+  float battery_voltage = battery_level*(5.0/1023.0);
+  char battery_string[10];
+  sprintf(battery_string, "%d", battery_voltage);
+  return client.publish("weather_display", (char*) battery_string);
+}
+
+void callback(char* topic, byte* payload, unsigned int length)
+{
+  Serial.print("New Message");
+  //Serial.print(topic);
+  //Serial.print(payload);
+  lcd.clear();
+  for (int i=0; i < length - 1; i++)
+  {
+    //Write payload to lcd, but allow for multiline display
+    if (i == 15)
+    {
+      lcd.setCursor(0,1);
+    }
+    lcd.write(payload[i]);
+  }  
+}
 
 void setup() 
 {
@@ -31,122 +125,10 @@ void setup()
 
 void loop() 
 {
-  while(!readFromServer())
+  if (!client.connected())
   {
-    Serial.println("Attempting Server Read");
-    delay(10*1000);
+    connectToServer();
   }
-  /*if (lastRead + readRate <= millis())
-  {
-    if (readFromServer())
-      lastRead = millis();
-  }*/
-  Serial.println("Entering sleep");
-  delay(100);
-  ESP.deepSleep(15 * 60 * 1000000, WAKE_RF_DEFAULT);
-  delay(100);
-}
-
-void connectWiFi()
-{
-  byte ledStatus = LOW;
-  mcMode = 1;
-
-  // Set WiFi mode to station (as opposed to AP or AP_STA)
-  WiFi.mode(WIFI_STA);
-
-  // WiFI.begin([ssid], [passkey]) initiates a WiFI connection
-  // to the stated [ssid], using the [passkey] as a WPA, WPA2,
-  // or WEP passphrase.
-  WiFi.begin(WiFiSSID, WiFiPSK);
-
-  // Use the WiFi.status() function to check if the ESP8266
-  // is connected to a WiFi network.
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    // Blink the LED
-    digitalWrite(LED_PIN, ledStatus); // Write LED high/low
-    ledStatus = (ledStatus == HIGH) ? LOW : HIGH;
-
-    // Delays allow the ESP8266 to perform critical tasks
-    // defined outside of the sketch. These tasks include
-    // setting up, and maintaining, a WiFi connection.
-    delay(5000); //wait 5 seconds
-    // Potentially infinite loops are generally dangerous.
-    // Add delays -- allowing the processor to perform other
-    // tasks -- wherever possible.
-    Serial.println("Not connected");
-    lcd.clear();
-    lcd.print("WiFi Not");
-    lcd.setCursor(0,1);
-    lcd.print("Connected");
-  }
-  //Serial.println("Wifi connected successfully");
-  lcd.clear();
-  lcd.print("WiFi Connected");
-  mcMode = 4;
-}
-
-void initHardware()
-{
-  Serial.begin(9600);
-  lcd.begin(16,2);
-  pinMode(LED_PIN, OUTPUT);
-}
-
-int readFromServer()
-{
-  // LED turns on when we enter, it'll go off when we 
-  // successfully post.
-  digitalWrite(LED_PIN, LOW);
-
-  WiFiClient client;
-  const int httpPort = 80;
-  if (!client.connect(serverHost, httpPort)) 
-  {
-    // If we fail to connect, return 0.
-    Serial.println("Failed to connect to server");
-    lcd.clear();
-    lcd.print("Server Not");
-    lcd.setCursor(0,1);
-    lcd.print("Connected1");
-    mcMode = 3;
-    delay(100);
-    return 0;
-  }
-  Serial.println("Connected to server");
-  // If we successfully connected, print our Phant post:
-  //client.print(phant.post());
-  client.println("GET /weather_data");
-
-  Serial.println("Send request");
-
-  delay(1000);
-
-//  char readInLine1[32] = "";
-//  char readInLine2[32] = "";
-  int i = 0;
-  while (client.available())
-  {
-    if (i < 16)
-      readInLine1[i] = client.read();
-    else
-      readInLine2[i-16] = client.read();
-    i += 1;
-  }
-//  String line1(readInLine1);
-//  String line2(readInLine2);
-  Serial.println(readInLine1);
-  Serial.println(readInLine2);
-  lcd.clear();
-  lcd.print(readInLine1);
-  lcd.setCursor(0,1);
-  lcd.print(readInLine2);
-  
-  // Before we exit, turn the LED off.
-  digitalWrite(LED_PIN, HIGH);
-  mcMode = 3;
-
-  return 1; // Return success
+  client.loop(); 
 }
 
